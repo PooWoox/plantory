1a042cda757d4463fb1766302c4c9697e2b49dff
Rodrigo <rodbove@gmail.com>
2020-12-19 11:18:22 -0300

    env set

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,9 @@
+import 'dotenv';
 import express from 'express';
 
 import routes from './routes';
+
+import './database';
 
 const app = express();
 
@@ -8,6 +11,5 @@ app.use(express.json());
 app.use(routes);
 
 app.listen(3333, () => {
-  // eslint-disable-next-line no-console
   console.log('Server started on port 3333!');
 });
